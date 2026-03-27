4ee4b206a06eb266ce9a8d1e5b164d72686b3c42
Ramimami7 <rhabbal22@gamil.com>
2026-03-26 22:24:23 -0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -9,21 +9,33 @@ int main()
     double factor = 0.1;
     double scholarship = 10000.0;
 
-    // Use the following input format: <status> <gender> <major>
-    // student female cs ; or student male math ; or nonstudent male cs ; etc.
     cin >> status >> gender >> major;
 
-    // TODO: Determine factor based on status, gender, and major using if statements
+    // Determine factor based on status, gender, and major using nested if statements
+    if (status == "student") {
+        factor = 0.2; // base student factor
+        if (gender == "female") {
+            factor = 1.0;
+            if (major == "cs") {
+                factor = 3.0;
+            }
+        } else {
+            // male or other
+            if (major == "cs") {
+                factor = 2.0;
+            }
+        }
+    } else {
+        // faculty or other
+        if (major == "cs") {
+            factor = 0.5;
+        }
+        // otherwise factor stays at default 0.1
+    }
 
     double finalScholarship = scholarship * factor;
 
     cout << setprecision(2) << fixed;
-    /* **************************************************
-    **************************************************
-    Do not change the output format. The output should be in the following format:
-    Decided Factor: <factor>, Final Scholarship Amount: <finalScholarship>
-    **************************************************
-    ************************************************** */
     cout << "Decided Factor: " << factor << ", Final Scholarship Amount: " << finalScholarship << endl;
 
     return 0;
